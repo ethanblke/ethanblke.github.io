a2d7f7b4784b9311e6a035bab4213a73ad4b33f5
Ethan Blake <lexiiboi@proton.me>
2025-09-18 12:28:29 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const animatedElements = document.querySelectorAll(".animate, .animated-section");
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if(entry.isIntersecting){
+        entry.target.classList.add("animate-in");
+      } else {
+        entry.target.classList.remove("animate-in");
+      }
+    });
+  }, {
+    threshold: 0.2
+  });
+
+  animatedElements.forEach(el => observer.observe(el));
+});
